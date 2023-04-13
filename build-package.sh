7e6ff4124ebf5046e7792ab57addb4e31587bdb9
Jeremy Greenwood <jagreenwood@gmail.com>
2023-04-13 15:47:04 -0400

    copy zip to output

--- build-package.sh
+++ build-package.sh
@@ -2,14 +2,23 @@
 
 EXECUTABLE=$1
 TARGET=".build/lambda/$EXECUTABLE"
+OUTPUT="output"
 
+rm -rf "$OUTPUT"
+mkdir -p "$OUTPUT"
+
+echo "Building Lambda"
 swift build --product $EXECUTABLE -c release
 
 rm -rf "$TARGET"
 mkdir -p "$TARGET"
 cp ".build/release/$EXECUTABLE" "$TARGET/"
 
+echo "Packaging Lambda"
 ldd ".build/release/$EXECUTABLE" | grep swift | awk '{print $3}' | xargs cp -Lv -t "$TARGET"
 cd "$TARGET"
 ln -s "$EXECUTABLE" "bootstrap"
 zip --symlinks lambda.zip *
+
+mv lambda.zip $OUTPUT
+echo "Done"
