89ea145a9709eabc7f95d7fba9cc782f3cd7ee2a
Jeremy Greenwood <jagreenwood@gmail.com>
2023-04-14 15:14:54 -0400

    mv to workspace
    This reverts commit 7b20779844f9e3f33daf7b55657bed95a49fd2ad.

--- build-package.sh
+++ build-package.sh
@@ -16,10 +16,6 @@
 
 EXECUTABLE=$1
 TARGET=".build/lambda/$EXECUTABLE"
-OUTPUT="output"
-
-rm -rf "$OUTPUT"
-mkdir -p "$OUTPUT"
 
 echo "Building Lambda"
 swift build --product "$EXECUTABLE" -c release
@@ -30,11 +26,9 @@ cp ".build/release/$EXECUTABLE" "$TARGET/"
 
 echo "Packaging Lambda"
 ldd ".build/release/$EXECUTABLE" | grep swift | awk '{print $3}' | xargs cp -Lv -t "$TARGET"
-pushp "$TARGET"
+cd "$TARGET"
 ln -s "$EXECUTABLE" "bootstrap"
 zip --symlinks lambda.zip *
 
-popd
-
-cp "$TARGET/lambda.zip" "$OUTPUT/"
+mv lambda.zip "$GITHUB_WORKSPACE/"
 echo "Done"
